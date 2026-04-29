8472a662c1ad4563379356200a0458173f416c3d
IRUTINGABO Aymar Audry <75417356+aymaraudry@users.noreply.github.com>
2026-04-29 05:10:13 -0700

    Refactor ESLint config to use Linter.Config format
    Refactor ESLint configuration to use Linter.Config array and include next plugin.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import type { Linter } from "eslint";
+import nextPlugin from "@next/eslint-plugin-next";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+const eslintConfig: Linter.Config[] = [
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: [".next/**", "out/**", "build/**", "node_modules/**"],
+  },
+];
 
 export default eslintConfig;
